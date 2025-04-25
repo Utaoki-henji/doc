d99bafa5cfc31cc706e98550847044485cb423d2
Utaoki-henji <61659526+Utaoki-henji@users.noreply.github.com>
2025-04-25 09:39:29 +0000

    Deploying to gh-pages from @ Utaoki-henji/doc@7af73d3258b18f121002683b77b0ebfbe29225bb 🚀

---
+++ assets/layout.html-swuRBmOn.js
@@ -0,0 +1,31 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,d as a,o}from"./app-BYoH1Wb7.js";const s={};function n(m,h){return o(),t("div",null,[a(` ---
+title: Layout
+icon: object-group
+order: 2
+category:
+  - Guide
+tag:
+  - Layout
+---
+
+The layout contains:
+
+- [Navbar](https://theme-hope.vuejs.press/guide/layout/navbar.html)
+- [Sidebar](https://theme-hope.vuejs.press/guide/layout/sidebar.html)
+- [Footer](https://theme-hope.vuejs.press/guide/layout/footer.html)
+
+Also each page can contain:
+
+- [BreadCrumb](https://theme-hope.vuejs.press/guide/layout/breadcrumb.html)
+- [Title and information](https://theme-hope.vuejs.press/guide/feature/page-info.html)
+- [TOC (Table of Contents)](https://theme-hope.vuejs.press/guide/layout/page.html#header-list)
+- [Meta information including update time and contributors](https://theme-hope.vuejs.press/guide/feature/meta.html)
+- [Comments](https://theme-hope.vuejs.press/guide/feature/comment.html)
+
+The theme also has the following elements:
+
+- [Dark mode button](https://theme-hope.vuejs.press/guide/interface/darkmode.html)
+- [Back to top button](https://theme-hope.vuejs.press/guide/interface/others.html#back-to-top-button)
+- [Print button](https://theme-hope.vuejs.press/guide/interface/others.html#print-button)
+
+You can customize them in theme options and page frontmatter. `)])}const u=e(s,[["render",n]]),l=JSON.parse('{"path":"/demo/layout.html","title":"","lang":"zh-CN","frontmatter":{},"git":{"createdTime":1744268954000,"updatedTime":1744618073000,"contributors":[{"name":"Utaoki.七月","username":"","email":"julys08@163.com","commits":4}],"changelog":[{"hash":"83d5ceeffbbafa712c44720adab3711b1a5aa742","time":1744618073000,"email":"julys08@163.com","author":"Utaoki.七月","message":"移除无用demo"},{"hash":"517d5f56652efb0936121ccfd003f1a4b09271e5","time":1744611118000,"email":"julys08@163.com","author":"Utaoki.七月","message":"调整目录结构"},{"hash":"aaa2a2b65b7f70bded401734725b6f686f008a6c","time":1744601136000,"email":"julys08@163.com","author":"Utaoki.七月","message":"调整路由，自定义评论配置，结构开发"},{"hash":"2bc28de2296a609d72317d6a6ede3f27f6f4c972","time":1744268954000,"email":"julys08@163.com","author":"Utaoki.七月","message":"init"}]},"readingTime":{"minutes":0.35,"words":106},"filePathRelative":"demo/layout.md","localizedDate":"2025年4月10日"}');export{u as comp,l as data};
